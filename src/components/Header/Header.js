5a6d1bd34d3080dfe8285ee95dfd661012c8946c
Arthur Badin <akherousin94@gmail.com>
2023-03-07 23:02:22 +0600

    feat: add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,7 +29,19 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <ActionGroupLaptop>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </ActionGroupLaptop>
         <Logo />
+        <SubscriptionGroup>
+          <Button>Subscribe</Button>
+          <Link href="">Already a subscriber?</Link>
+        </SubscriptionGroup>
       </MainHeader>
     </header>
   );
@@ -39,6 +51,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media (${QUERIES.laptopAndUp}) {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +75,57 @@ const ActionGroup = styled.div`
   }
 `;
 
+const ActionGroupLaptop = styled(ActionGroup)`
+  display: none;
+
+  @media (${QUERIES.laptopAndUp}) {
+    display: flex;
+  }
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media (${QUERIES.tabletAndUp}) {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+
+  @media (${QUERIES.laptopAndUp}) {
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    margin-top: 16px;
+    margin-bottom: 83px;
+  }
+`;
+
+const SubscriptionGroup = styled.div`
+  position: relative;
+  display: none;
+  justify-self: end;
+  align-items: center;
+  gap: 8px;
+
+  @media (${QUERIES.laptopAndUp}) {
+    display: revert;
+  }
+`;
+
+const Link = styled.a`
+  position: absolute;
+
+  font-size: calc(14rem / 16);
+  text-decoration: underline;
+  font-style: italic;
+  color: var(--color-gray-900);
+
+  width: 100%;
+  text-align: center;
+  margin-block-start: 8px;
 `;
 
 export default Header;
